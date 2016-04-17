f209581f584bb981b186bebcdbd2e10e656e88c6
Jace Browning <jacebrowning@gmail.com>
2016-04-17 19:12:29 -0400

    Allow <0.1 variation in metrics
    Closes #23

--- tests/test_views_root.py
+++ tests/test_views_root.py
@@ -7,14 +7,9 @@ from .utilities import load
 
 def describe_root():
 
-    def it_returns_metadata(client, monkeypatch):
-        monkeypatch.setenv('DEPLOY_DATE', "today")
+    def it_returns_metadata(client):
 
         status, data = load(client.get("/"))
 
         expect(status) == 200
-        expect(data) == {
-            'version': "0.2",
-            'date': "today",
-            'changes': "https://raw.githubusercontent.com/jacebrowning/coverage-space/master/CHANGES.md"
-        }
+        expect(sorted(data.keys())) == ['changes', 'date', 'version']
