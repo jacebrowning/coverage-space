7ce8c7ce159ff8d39e323fe57ad8463c221cedd3
Jace Browning <jacebrowning@gmail.com>
2020-12-29 15:53:24 -0500

    Update Python to 3.6.10

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -7,18 +7,7 @@ import yorm
 def pytest_configure(config):
     """Disable verbose output when running tests."""
     terminal = config.pluginmanager.getplugin('terminal')
-    base = terminal.TerminalReporter
-
-    class QuietReporter(base):
-        """A py.test reporting that only shows dots when running tests."""
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.verbosity = 0
-            self.showlongtestinfo = False
-            self.showfspath = False
-
-    terminal.TerminalReporter = QuietReporter
+    terminal.TerminalReporter.showfspath = False
 
 
 def pytest_runtest_setup(item):
